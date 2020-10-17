85cdbcaa6322d10995d2b1e7eb6dd24b684dfb04
Huong Devecsery <hndathome@gmail.com>
2020-10-17 12:31:46 -0500

    reorg code move hbs helper functions to separate file

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ require('./db');
 configPassport(passport);
 
 const errorHandler = require('errorhandler');
-const lineChart = require('./line-chart');
 
 const app = express();
 app.use(session({
@@ -25,42 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.engine('hbs', hbs({
     extname: 'hbs',
-    helpers: {
-        getPrettyNumber: function (number) {
-            if (number === null) {
-                return "no data is available"
-            }
-            return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        },
-        getUpDown: function (number) {
-            let str = "";
-            if (number) {
-                if (number > 0) {
-                    str = `<i class="fa fw fa-caret-up increase"></i>`
-                }
-                else {
-                    str = `<i class="fa fw fa-caret-down decrease"></i>`
-                }
-            }
-            return str;
-        },
-        getPlusMinus: function (number) {
-            let str = "";
-            if (number) {
-                if (number > 0) {
-                    str = `<i class="fa fw fa-plus decrease"></i>`
-                }
-                else {
-                    str = `<i class="fa fw fa-minus increase"></i>`
-                }
-            }
-            return str;
-        },
-        graphics: function () {
-            let str = lineChart.createSVG();
-            return str;
-        }
-    }
+    helpers: require('./public/js/handlebars-helpers')
 }));
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
---
+++ public/js/handlebars-helpers.js
@@ -0,0 +1,38 @@
+const lineChart = require('../../line-chart');
+
+module.exports = {
+    getPrettyNumber: function (number) {
+        if (number === null) {
+            return "no data is available"
+        }
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    },
+    getUpDown: function (number) {
+        let str = "";
+        if (number) {
+            if (number > 0) {
+                str = `<i class="fa fw fa-caret-up increase"></i>`
+            }
+            else {
+                str = `<i class="fa fw fa-caret-down decrease"></i>`
+            }
+        }
+        return str;
+    },
+    getPlusMinus: function (number) {
+        let str = "";
+        if (number) {
+            if (number > 0) {
+                str = `<i class="fa fw fa-plus decrease"></i>`
+            }
+            else {
+                str = `<i class="fa fw fa-minus increase"></i>`
+            }
+        }
+        return str;
+    },
+    graphics: function () {
+        let str = lineChart.createSVG();
+        return str;
+    }
+}
