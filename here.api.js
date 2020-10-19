aa2ea44eea35d37d9ed8f257c015022dbe3ea59a
Huong Devecsery <hndathome@gmail.com>
2020-10-18 21:39:43 -0500

    save an array of lookup objects vs the last lookup

--- here.api.js
+++ here.api.js
@@ -1,8 +1,7 @@
 //`https://discover.search.hereapi.com/v1/discover?apikey={{apiKey}}&q=Covid&at=${gps}&limit=10` covid-19 testing locations
 const axios = require('axios');
-let lastGPS;
-let hereData;
-let lastDay;
+let hereArray = [];
+let gpsMarkers;
 
 const formatPhoneNumber = (phoneNumberString) => {
     let cleaned = ('' + phoneNumberString).replace(/\D/g, '')
@@ -17,8 +16,12 @@ const formatPhoneNumber = (phoneNumberString) => {
 async function getCovid19TestingLocations(gps) {
     let today = new Date();
     today.setHours(0, 0, 0, 0);
+    const todayString = today.toString();
+    let indexFound = -1;
     try {
-        if (gps != lastGPS || hereData === undefined || lastDay === undefined || today != lastDay) {
+        indexFound = hereArray.findIndex(obj => obj.gps === gps);
+
+        if (hereArray.length === 0 || indexFound === -1 || todayString != hereArray[indexFound].lastDay) {
             const url = `https://discover.search.hereapi.com/v1/discover?apikey=${process.env.HERE_API_KEY}&q=Covid&at=${gps}&limit=10`;
             const response = await axios.get(url, { headers: { 'Accept': 'application/json' } });
 
@@ -26,15 +29,25 @@ async function getCovid19TestingLocations(gps) {
             const dataMarkers = items.filter(current => current.title.startsWith("Covid-19 Testing Site")).reduce((accumulator, current) => {
                 return [...accumulator, { markerText: current.title.slice(23), latitude: current.position.lat, longitude: current.position.lng, phone: current.contacts[0].phone[0].value, formatPhone: formatPhoneNumber(current.contacts[0].phone[0].value), address: current.address }]
             }, []);
-            hereData = dataMarkers;
+            gpsMarkers = dataMarkers;
+        }
+        else {
+            gpsMarkers = hereArray[indexFound].dataMarkers;
         }
     } catch (error) {
-        hereData = undefined;
+        gpsMarkers = undefined;
         console.error(error);
     }
-    lastDay = today;
-    lastGPS = gps;
-    return hereData;
+
+    if (indexFound === -1) {
+        hereArray.push({ gps: gps, lastDay: todayString, dataMarkers: [...(gpsMarkers || [])] })
+    }
+    else {
+        hereArray[indexFound].dataMarkers = [...(gpsMarkers || [])];
+        hereArray[indexFound].lastDay = todayString;
+    }
+
+    return gpsMarkers;
 }
 
 module.exports = {
