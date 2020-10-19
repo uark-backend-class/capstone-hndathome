711e973c79c6cdfee9781b39f4709e7cb144092f
Huong Devecsery <hndathome@gmail.com>
2020-10-18 22:44:43 -0500

    lookup array instead of single lookup object for smartystreets

--- smartystreets.api.js
+++ smartystreets.api.js
@@ -1,17 +1,30 @@
 //`https://us-zipcode.api.smartystreets.com/lookup?auth-id=YOUR+AUTH-ID+HERE&auth-token=YOUR+AUTH-TOKEN+HERE&zipcode=${zipCode}` zip code validation
 const axios = require('axios');
-let lastZipCode;
+let zipCodeArray = [];
 let zipCodeData;
 async function lookupZipCode(zipCode) {
+    let indexFound = -1;
     try {
-        if (zipCode != lastZipCode || zipCodeData === undefined) {
+        indexFound = zipCodeArray.findIndex(obj => obj.zipCode === zipCode);
+
+        if (zipCodeArray.length === 0 || indexFound === -1) {
             const url = `https://us-zipcode.api.smartystreets.com/lookup?auth-id=${process.env.SS_AUTH_ID}&auth-token=${process.env.SS_AUTH_TOKEN}&zipcode=${zipCode}`;
             const response = await axios.get(url, { headers: { 'Accept': 'application/json' } });
             zipCodeData = response.data[0];
         }
+        else {
+            zipCodeData = { ...zipCodeArray[indexFound].zipCodeData };
+        }
     } catch (error) {
         zipCodeData = undefined;
         console.error(error);
+    }
+
+    if (indexFound === -1) {
+        zipCodeArray.push({ zipCode: zipCode, zipCodeData: { ...zipCodeData } })
+    }
+    else {
+        zipCodeArray[indexFound].zipCodeData = { ...zipCodeData };
     }
     lastZipCode = zipCodeData;
     return zipCodeData;
